0b20b19e6754ad1a2413fe4ad817810606ab10a5
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2018-02-26 17:11:29 +0300

    Refactor code to answer question

--- strong_password.js
+++ strong_password.js
@@ -1,22 +1,34 @@
 const strongPassword = (n, password) => {
-    const numbers = "0123456789";
-    const lower_case = "abcdefghijklmnopqrstuvwxyz";
-    const upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    const special_characters = "!@#$%^&*()-+";
-    let characters = 0;
-    if (!check(password.split(''), numbers.split(''))) {
-        characters += 1;
+    if (1 <= n && n <= 100 && n === password.length) {
+        const numbers = /\d/;
+        const lower_case = /[a-z]/;
+        const upper_case = /[A-Z]/;
+        const special_characters = /[!@#$%^&*()-+]/;
+        let characters = 0;
+        if (!numbers.test(password)) {
+            characters += 1;
+        }
+        if (!lower_case.test(password)) {
+            characters += 1;
+        }
+        if (!upper_case.test(password)) {
+            characters += 1;
+        }
+        if (!special_characters.test(password)) {
+            characters += 1;
+        }
+        if (n < 6) {
+            let missing = 6 - n;
+            if (characters < missing) {
+                console.log(missing);
+            }
+            else {
+                console.log(characters);
+            }
+        } else {
+            console.log(characters);
+        }
     }
-    if (!check(password.split(''), lower_case.split(''))) {
-        characters += 1;
-    }
-    if (!check(password.split(''), upper_case.split(''))) {
-        characters += 1;
-    }
-    if (!check(password.split(''), special_characters.split(''))) {
-        characters += 1;
-    }
-    console.log(characters);
 }
 
-strongPassword(3, 'Ab1');
+strongPassword(11, '#HackerRank');
