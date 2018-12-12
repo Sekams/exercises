7e0a0595710c6f89de693537a62084fc6fa5ba71
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2018-12-12 16:20:27 +0300

    Refactor question 2

--- softcom_eng_tests_question_2.js
+++ softcom_eng_tests_question_2.js
@@ -1,9 +1,8 @@
 const solution = (A, K) => {
-  for (let index = 0; index <= K; index++) {
-    if (index > 0) {
-      A.unshift(A[A.length - 1]);
-      A.pop();
-    }
+  for (let index = 1; index <= K; index++) {
+    A.unshift(A.pop());
   }
   return A;
 }
+
+console.log(solution([3, 8, 9, 7, 6], 1));
